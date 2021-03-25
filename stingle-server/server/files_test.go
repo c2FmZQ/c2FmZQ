85563af134e866ac6a813b458aa24a1a51457416
Robin Thellend <rthellend@gmail.com>
2021-03-24 18:29:53 -0700

    Add test for /v2/sync/getDownloadUrls

--- stingle-server/server/files_test.go
+++ stingle-server/server/files_test.go
@@ -80,7 +80,40 @@ func TestUploadDownload(t *testing.T) {
 	}
 
 	// Download with /v2/sync/getDownloadUrls
-	// ...
+	files := []string{"filename1", "filename2"}
+	sets := []string{database.GallerySet, database.AlbumSet}
+	urls, err := c.getDownloadURLs(files, sets, false)
+	if err != nil {
+		t.Errorf("c.getDownloadURLs(%v, %v, false) failed: %v", files, sets, err)
+	}
+	for _, f := range []struct{ filename, body string }{
+		{"filename1", `Content of "file" filename "filename1"`},
+		{"filename2", `Content of "file" filename "filename2"`},
+	} {
+		body, err := c.downloadGet(urls[f.filename])
+		if err != nil {
+			t.Fatalf("c.downloadGet(%q) failed: %v", urls[f.filename], err)
+		}
+		if want, got := f.body, body; want != got {
+			t.Errorf("c.downloadGet returned unexpected body: Want %q, got %q", want, got)
+		}
+	}
+
+	if urls, err = c.getDownloadURLs(files, sets, true); err != nil {
+		t.Errorf("c.getDownloadURLs(%v, %v, true) failed: %v", files, sets, err)
+	}
+	for _, f := range []struct{ filename, body string }{
+		{"filename1", `Content of "thumb" filename "filename1"`},
+		{"filename2", `Content of "thumb" filename "filename2"`},
+	} {
+		body, err := c.downloadGet(urls[f.filename])
+		if err != nil {
+			t.Fatalf("c.downloadGet(%q) failed: %v", urls[f.filename], err)
+		}
+		if want, got := f.body, body; want != got {
+			t.Errorf("c.downloadGet returned unexpected body: Want %q, got %q", want, got)
+		}
+	}
 }
 
 func (c *client) getURL(file, set string) (string, error) {
@@ -100,4 +133,32 @@ func (c *client) getURL(file, set string) (string, error) {
 		return "", fmt.Errorf("server did not return a url: %v", sr.Parts["url"])
 	}
 	return url, nil
+}
+
+func (c *client) getDownloadURLs(files, sets []string, isThumb bool) (map[string]string, error) {
+	form := url.Values{}
+	form.Set("token", c.token)
+	if isThumb {
+		form.Set("is_thumb", "1")
+	}
+	for i := range files {
+		form.Set(fmt.Sprintf("files[%d][filename]", i), files[i])
+		form.Set(fmt.Sprintf("files[%d][set]", i), sets[i])
+	}
+	sr, err := c.sendRequest("/v2/sync/getDownloadUrls", form)
+	if err != nil {
+		return nil, err
+	}
+	if sr.Status != "ok" {
+		return nil, fmt.Errorf("status:nok %+v", sr)
+	}
+	urls, ok := sr.Parts["urls"].(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("server did not return a list of urls: %#v", sr.Parts["urls"])
+	}
+	out := make(map[string]string)
+	for k, v := range urls {
+		out[k] = v.(string)
+	}
+	return out, nil
 }
