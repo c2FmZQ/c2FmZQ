57262bdabf5a0a1ea421d46fb6cf3926d81021e3
Robin Thellend <rthellend@gmail.com>
2023-02-10 07:51:38 -0800

    Bump version to v0.4.0

--- c2FmZQ/internal/pwa/version.js
+++ c2FmZQ/internal/pwa/version.js
@@ -18,7 +18,7 @@
 
 'use strict';
 
-const VERSION = 'v0.3.12';
+const VERSION = 'v0.4.0';
 
 // Indicates that the PWA is only allowed to connect to the server from which
 // it was loaded. Set to false to allow cross-origin connections.
