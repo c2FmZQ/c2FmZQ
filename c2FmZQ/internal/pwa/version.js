2d6d7bcc7eda2c858586e360a177d40982fbd76d
Robin Thellend <rthellend@gmail.com>
2023-12-28 09:35:45 -0800

    v0.4.17

--- c2FmZQ/internal/pwa/version.js
+++ c2FmZQ/internal/pwa/version.js
@@ -18,7 +18,7 @@
 
 'use strict';
 
-const VERSION = 'v0.4.16';
+const VERSION = 'v0.4.17';
 
 // Indicates that the PWA is only allowed to connect to the server from which
 // it was loaded. Set to false to allow cross-origin connections.
